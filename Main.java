30eaa91da9f1cfac78e9a63751dbadc3ffa75af0
szczyputek <wojciech.stracilo@gmail.com>
2016-04-13 22:00:15 +0200

    Added files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+package pl.pgs.tree;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner odczyt = new Scanner(System.in);
+
+		
+		//System.out.println("Podaj znak: ");
+		//String znak = odczyt.nextLine();
+		String znak="x";
+		
+		//System.out.println("Podaj kierunek ( lewo - left, œrodek - up, prawo - right, dol-down ) : ");
+		//String kierunek = odczyt.nextLine();
+		String kierunek="up";
+		
+		System.out.println("Podaj ilosc: ");
+		int ilosc = odczyt.nextInt();
+		
+		TreeCreator tree=new TreeCreator();
+	
+		System.out.println(tree.tree(ilosc, znak, kierunek));
+		
+		odczyt.close();
+		
+
+	}
+
+}
---
+++ TreeCreator.java
@@ -0,0 +1,92 @@
+package pl.pgs.tree;
+
+public class TreeCreator {
+	
+	
+	private StringBuilder add(int length, String sign) {
+		StringBuilder stringbuilder = new StringBuilder();
+
+		for (int i = 0; i < length; i++) {
+			stringbuilder.append(sign);
+		}
+
+		return stringbuilder;
+	}
+	
+	private String printlevel(int nOflevels, int currentlevel, String sign, String direction) {
+
+		String level = "";
+
+		switch (direction) {
+		case "up": {
+			level += add(nOflevels - currentlevel - 1, " ");
+			level += add(2 * currentlevel + 1, sign);
+			break;
+		}
+
+		case "right": {
+			level += add(2 * currentlevel + 1, sign);
+			break;
+		}
+//current-aktualny(idzie od 0), nOflevels-liczba poziomow(od uzytkownika)
+		case "left": {
+			if(currentlevel<nOflevels){
+			level += add(nOflevels - currentlevel-1, " ");
+			level += add(currentlevel + 1, sign);
+			}
+			if(currentlevel==nOflevels)
+				{
+				level+="\n";}
+			if(currentlevel>=nOflevels){
+			
+				level+= add(currentlevel-nOflevels+1," ");
+				level+= add(2*nOflevels-currentlevel-1,sign);
+				//level+= add(nOflevels-currentlevel+3, sign);
+			 }
+			break;
+		}
+		
+		case "down": {
+			level += add(nOflevels - currentlevel-1, " ");
+			level += add(currentlevel + 1, sign);
+			
+			break;
+		}
+
+		default:
+			System.out.println("Poda³eœ z³y znak !");
+		}
+		
+
+		if (currentlevel + 1 != nOflevels) {
+			level += "\n";
+		}
+
+		return level;
+	}
+
+	
+	
+	public  String tree(int nOflevel, String sign, String direction) {
+		String tree = "";
+		
+		if (direction.equals("up")|| direction.equals("down")){
+		for (int i = 0; i < nOflevel; i++) {
+			{
+				tree += printlevel(nOflevel, i, sign, direction);
+			}
+		}}
+		
+		else if (direction.equals("left")|| direction.equals("rigth"))
+		{
+			for (int i = 0; i < 2*nOflevel; i++) {
+				{
+					tree += printlevel(nOflevel, i, sign, direction);
+				}
+			}	
+		}
+		return tree;
+
+	}
+
+}
